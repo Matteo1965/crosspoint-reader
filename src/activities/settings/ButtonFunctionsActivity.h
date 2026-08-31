699c11406b3ac004d7f60c7a870e7ded84179b5a
Matteo <atafoto@gmail.com>
2026-08-31 16:10:35 +0200

    Add button functions settings activity

---
+++ src/activities/settings/ButtonFunctionsActivity.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "ReaderAction.h"
+#include "ReaderButtonProfile.h"
+#include "activities/UiListActivity.h"
+#include "components/OptionPopup.h"
+
+class ButtonFunctionsActivity final : public UiListActivity {
+ public:
+  ButtonFunctionsActivity(GfxRenderer& renderer, MappedInputManager& mappedInput);
+  void onEnter() override;
+
+ protected:
+  int listCount() const override { return 12; }
+  void buildScreen(UiScreen& screen) override;
+  void activateIndex(int index) override;
+  bool handleCustomInput() override;
+  const char* headerTitle() const override;
+
+ private:
+  static ReaderPhysicalButton buttonForRow(int row);
+  static ReaderButtonGesture gestureForRow(int row);
+  static const char* actionLabel(ReaderAction action);
+  void rebuildRows();
+  void openActionPicker(int row);
+
+  OptionPopup optionPopup_;
+  std::vector<std::string> labels_;
+  std::vector<std::string> values_;
+  std::vector<freeink::ui::ListItem> rows_;
+};
